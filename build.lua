765e56b0dc552cca9af06b606f8a7c9437c1e05e
SLACLAU <seb.laclau@gmail.com>
2022-02-11 23:14:50 +0000

    Update build.lua
    Added uploadconfig table

--- build.lua
+++ build.lua
@@ -2,3 +2,11 @@ module = "articletobook"
 maindir = "."
 sourcefiles = {"*.dtx", "*.ins", "ltxdoc-commands.def"}
 packtdszip = true
+uploadconfig = {
+  pkg         = "articletobook",
+  author      = "Sebastien Laclau"
+  license     = "lppl1.3c",
+  summary     = "This package converts article based classes to book based classes.",
+  ctanPath    = "/macros/latex/contrib/articletobook",
+  repository  = "https://github.com/laclaus/articletobook",
+}
