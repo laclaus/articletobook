e512f4ea0d2202c7ec192fb284cdf5ed94ec700d
slaclau <seb.laclau@gmail.com>
2022-02-22 13:54:23 +0000

    Update to build and gitignore

--- build.lua
+++ build.lua
@@ -4,5 +4,9 @@ sourcefiles = {"*.dtx", "*.ins", "ltxdoc-commands.def"}
 packtdszip = true
 uploadconfig = {
   pkg         = "articletobook",
-  author      = "Sebastien Laclau"
+  author      = "Sebastien Laclau",
+  license     = "lppl1.3c",
+  ctanPath    = "/macros/latex/contrib/articletobook",
+  repository  = "https://github.com/slaclau/articletbook/",
+  summary     = "Converts article based classes to book based classes.",
 }
